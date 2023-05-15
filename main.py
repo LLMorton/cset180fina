b8fd2b5c013c3a2cc13ae7e279767e92e2717736
morto <www.llynnmorton88@gmail.com>
2023-05-15 09:24:08 -0400

    trying to display accounts and fix up my accounts page

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ conn_str = "mysql://root:lm091702@localhost/cset180"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
+app.config['SECRET_KEY'] = 'fireagatha2413.'
+
 
 def q(str):
     alph = "abcdefghijklmnopqrstuvwxyz"
@@ -13,32 +15,6 @@ def q(str):
     for char in str:
         sum += alph.find(char) + 1
     return sum
-
-
-@app.route('/login')
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        acc_type = request.form['acc_type']
-
-        query = text("select * from accounts where username = :username AND password= :password and acc_type= :acc_type")
-        params = {"username": username, "password": password,  "acc_type": acc_type}
-        with engine.connect() as conn:
-            user = conn.execute(query, params).fetchone()
-        if user is None:
-            return redirect(url_for('login'))
-        else:
-            return render_template('index.html')
-
-
-@app.route('/login/logout')
-def logout(none=None):
-    session.pop('logged in', none)
-    session.pop('id', none)
-    session.pop('username', none)
-    
-    return redirect(url_for('login'))
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -60,17 +36,60 @@ def registration():
         return render_template("base.html")
 
 
+@app.route('/login')
+def login():
+    if request.method == ['GET', 'POST']:
+        username = request.form['username']
+        password = request.form['password']
+        acc_type = request.form['acc_type']
+
+        query = text("select * from accounts where username = :username AND password= :password and acc_type= :acc_type")
+        params = {"username": username, "password": password,  "acc_type": acc_type}
+        with engine.connect() as conn:
+            user = conn.execute(query, params).fetchone()
+        if user is None:
+            return redirect(url_for('login'))
+        else:
+            return render_template('index.html')
+
+
+@app.route('/login/logout')
+def logout(none=None):
+    session.pop('logged in', none)
+    session.pop('id', none)
+    session.pop('username', none)
+
+    return redirect(url_for('login'))
+
+
 @app.route("/dashboard")
 def dash():
     return render_template("dashboard.html")
 
 
-@app.route("/login/home")
+@app.route("/home")
 def home():
+    query = text("SELECT * FROM accounts")
+    result = conn.execute(query)
+    accounts = []
+    for row in result:
+        accounts.append(row)
     return render_template("homepage.html")
 
 
-@app.route('/Products')
+@app.route('/accounts')
+def get_accounts():
+    accs = conn.execute(text("select * from accounts")).all()
+    query = text("SELECT * FROM accounts")
+    result = conn.execute(query)
+    accounts = []
+    for row in result:
+        accounts.append(row)
+
+    return render_template('accounts.html', accounts=accs)
+
+
+@app.route('/products')
 def products():
     query = text("SELECT * FROM products")
     with engine.connect() as conn:
